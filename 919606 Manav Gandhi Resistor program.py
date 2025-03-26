744558ab6999e2dae00b856bd634eb6e068677dc
Manav_gandhi <manav_gandhi@paraparaumucollege.school.nz>
2025-03-27 11:57:57 +1300

    Add boundrary code, using vs3 list and vs5 list, and try except.

--- 919606 Manav Gandhi Resistor program.py
+++ 919606 Manav Gandhi Resistor program.py
@@ -30,6 +30,8 @@ class Quiz:
         self.resistance_guess = 0 # the R value they guess
         self.resistance_calculate = 0 # the R value the code guesses
         self.led_data = led_data # data
+        self.vs3_list = ['3', '3V']
+        self.vs5_list = ['5', '5V']
 
     def clear(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -58,38 +60,51 @@ class Quiz:
 
     def guidelines(self):
         # Provides guidelines to the user on how the quiz works\
+
         index = 0
         self.clear()
         self.select_led(index)
-        self.voltage_supply = int(input("What supply voltage out of 3V or 5V would you like to pick?"))
-        if self.voltage_supply == 3 or self.voltage_supply == 5:
-            self.calculate_resistance()
-            print(f"The Supply voltage is {self.voltage_supply}V \nThe forward voltage is {self.voltage_forward}V"
-                  f"\nThe forward current is {self.current}mA")
+        while True:
+            try:
+                self.voltage_supply = input("What supply voltage out of 3V or 5V would you like to pick?")
 
-            self.pause()
+                if self.voltage_supply in self.vs3_list:
+                    self.voltage_supply = 3
+                    break
+                elif self.voltage_supply in self.vs5_list:
+                    self.voltage_supply = 5
+                    break
+                else:
+                    print("That is not 3V or 5V, please lock in")
+            except ValueError:
+                print("Invailid input, please lock in")
 
-            print(
-                "These peices of data are used to find the reccommend resistor in a series circuit(ie; for a jackbord)"
-                "\nThe formula required for this calculation is Ohm's Law, allowing you to calculate the resistance."
-                "\nA quick tip is that current(I) is measured in amps, the data you recieve for forward current"
-                "\nis in milliamps. To find the current in amps divide the current given by 1000."
-                "\n\nSo the formula requried to find the resistance is""\nResistance = (Vs - Vf) ÷ (If ÷ 1000)"
-            )
-            self.pause()
+        self.calculate_resistance()
+        print(f"The Supply voltage is {self.voltage_supply}V \nThe forward voltage is {self.voltage_forward}V"
+              f"\nThe forward current is {self.current}mA")
+
+        self.pause()
+
+        print(
+            "These peices of data are used to find the reccommend resistor in a series circuit(ie; for a jackbord)"
+            "\nThe formula required for this calculation is Ohm's Law, allowing you to calculate the resistance."
+            "\nA quick tip is that current(I) is measured in amps, the data you recieve for forward current"
+            "\nis in milliamps. To find the current in amps divide the current given by 1000."
+            "\n\nSo the formula requried to find the resistance is""\nResistance = (Vs - Vf) ÷ (If ÷ 1000)"
+        )
+        self.pause()
 
 
-            print(f'First of all we should divide the current by 1000 in order to find the current in amps'
-                  f'\n{self.current} / 1000 = {self.current_amps}A'
-                  f'\n\nNext wee need to add the supply voltage, forward voltage, and current into our previous equation'
-                  f'\nResistance = {self.voltage_supply} - {self.voltage_forward} / {self.current_amps}'
-                  f'\nResitance = {self.resistance_calculate}Ω'
-                  f'\n\nWe will want the resistance to be rounded to 2 decimal places, altough values with <(2d.p.)'
-                  f'\ndo not need to be rounded'
-                  f'\n\nThus the resistance found is {self.resistance_calculate}Ω')
-            self.pause()
-        else:
-            print("That is not 3V or 5V, please lock in")
+        print(f'First of all we should divide the current by 1000 in order to find the current in amps'
+              f'\n{self.current} / 1000 = {self.current_amps}A'
+              f'\n\nNext wee need to add the supply voltage, forward voltage, and current into our previous equation'
+              f'\nResistance = {self.voltage_supply} - {self.voltage_forward} / {self.current_amps}'
+              f'\nResitance = {self.resistance_calculate}Ω'
+              f'\n\nWe will want the resistance to be rounded to 2 decimal places, altough values with <(2d.p.)'
+              f'\ndo not need to be rounded'
+              f'\n\nThus the resistance found is {self.resistance_calculate}Ω')
+        self.pause()
+
         enter = input("Press enter to continue")
         if enter == enter:
             self.clear()
@@ -107,13 +122,22 @@ class Quiz:
 
     def voltage_input(self):
         # Gathers the inputs from the user
-        self.voltage_supply = int(input("What supply voltage out of 3V or 5V would you like to pick?"))
-        if self.voltage_supply == 3 or self.voltage_supply == 5:
-            self.resistance_guess = int(input("What is the resistance "))
-        else:
-            print("That is not 3V or 5V, please lock in")
-            self.voltage_input()
+        while True:
+            try:
+                self.voltage_supply = input("What supply voltage out of 3V or 5V would you like to pick?")
 
+                if self.voltage_supply in self.vs3_list:
+                    self.voltage_supply = 3
+                    break
+                elif self.voltage_supply in self.vs5_list:
+                    self.voltage_supply = 5
+                    break
+                else:
+                    print("That is not 3V or 5V, please lock in")
+            except ValueError:
+                print("Invailid input, please lock in")
+
+        self.resistance_guess = int(input("What is the resistance "))
 
     def calculate_resistance(self):
         # Calculates resistance of the given question
